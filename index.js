680e7cb2d804693159a28a6c8564de59426ab954
Mehedi Hassan <meh67719@gamil.com>
2026-05-31 20:54:20 +0600

    connect wih mongodb

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000;
+
+
+const uri = "mongodb+srv://Edubridge:dHJeEJztWLgKNsP4@cluster0.jc6c5mw.mongodb.net/?appName=Cluster0";
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,3 +39,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
