bf56c73129c2e9feb43f7cd13b16138eaeda91ee
Mehedi Hassan <meh67719@gamil.com>
2026-06-27 21:43:48 +0600

    notice delete function create

--- Router/Notice/myuploadednotice.js
+++ Router/Notice/myuploadednotice.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { ObjectId } = require("mongodb");
+
 module.exports = (noticeCollection) => {
   const router = express.Router();
 
@@ -16,6 +18,47 @@ module.exports = (noticeCollection) => {
       res.status(500).send({ message: error.message });
     }
   });
+
+
+
+
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).send({
+        success: false,
+        message: "ID is required",
+      });
+    }
+
+    const result = await noticeCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).send({
+        success: false,
+        message: "Notice not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Notice deleted successfully",
+      deletedCount: result.deletedCount,
+    });
+  } catch (error) {
+    console.error("Delete Notice Error:", error);
+
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
   return router;
 };
